d94608298c196e7f7044dfcfc4bf092616a9f81d
paneedovanie <paneedovanie@yahoo.com>
2023-05-28 18:10:58 +0800

    Converted the server.js to server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
+import express from "express";
+import serveStatic from "serve-static";
+import path from "path";
 
 const app = express();
 
@@ -8,7 +8,7 @@ const app = express();
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 // this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
+app.get(/.*/, function (_, res) {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
